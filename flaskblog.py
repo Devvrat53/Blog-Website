14756199633b646139e2c205c78b2a1a482fc4ec
Devvrat53 <devvratmungekar53@gmail.com>
2020-04-24 22:34:02 +0530

    update

--- flaskblog.py
+++ flaskblog.py
@@ -5,7 +5,7 @@ from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4f73487168a9c36e5384f13525a0a01b'
-app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite.////site.db' # site.db will be created in the same directory in which we are working!
+app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite.///site.db' # site.db will be created in the same directory in which we are working!
 # With SQLAlchemy, we can store databases as classes
 db = SQLAlchemy(app)
 
